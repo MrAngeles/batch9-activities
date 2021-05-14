2ed8a4b75c7fc4d321c22f4cd1f1e24bf7d5f31e
Don Gerry Angeles <dongerryangeles05@gmail.com>
2021-05-14 23:49:59 +0800

    finished js coding challenge

--- angeles-don/js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/whats-in-the-fridge/app.js
+++ angeles-don/js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/whats-in-the-fridge/app.js
@@ -3,21 +3,35 @@ const upButton = document.querySelector('#up')
 const buyListDisplay = document.querySelector('#first-list')
 const fridgeListDisplay = document.querySelector('#second-list')
 
-const buyList = ['chicharon', 'buko pie', 'mango', 'bacon' ]
+const buyList = ['chicharon', 'buko pie', 'mango', 'bacon']
 const fridge = []
 
 //Challenge: Please fill the fridge array with 5 items of your choice. 
 
+fridge.push ('hotdog', 'chicken nuggets', 'ice tubig', 'siomai', 'beef')
+
 
 //Challenge 2: You have bought some chicharon.
 //Please remove it from the buyList and add it to the items in fridge.
-
+fridge.push(buyList[0]);
+buyList.shift();
 
 //Challenge 3: Write a function that will remove an item from the fridge,
 //and put it in the buyList, on the press of the moveUp button.
 
 function moveUp(){
     //your code
+    if (fridge.length !== 0) {
+        buyList.push(fridge[0]);
+        fridge.shift();
+
+        buyListDisplay.innerHTML = buyList;
+        fridgeListDisplay.innerHTML = fridge;
+    }
+    else
+    {
+        alert('No more items in the fridge!')
+    }
 }
 
 upButton.addEventListener('click', moveUp)
@@ -27,6 +41,17 @@ upButton.addEventListener('click', moveUp)
 
 function moveDown(){
     //your code
+    if (buyList.length !== 0) {
+        fridge.push(buyList[buyList.length - 1]);
+        buyList.pop();
+
+        buyListDisplay.innerHTML = buyList;
+        fridgeListDisplay.innerHTML = fridge;
+    }
+    else
+    {
+        alert('No more items to put in the fridge!')
+    }
 }
 
 downButton.addEventListener('click', moveDown)
