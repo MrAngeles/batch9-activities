23cdb75f33ba349621d77b5cc49b06891dffbdf1
Don Gerry Angeles <dongerryangeles05@gmail.com>
2021-05-20 19:55:36 +0800

    momentun-app: finish project

---
+++ angeles-don/momentum-app/javaScript/greeting.js
@@ -0,0 +1,36 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+function askForName(){
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+function paintGreeting(text){
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Good day to you, ${text}`;
+  document.querySelector(".js-form").style.display = "none";
+  greeting.style.display = 'block';
+}
+
+greeting.addEventListener('dblclick', function(){
+  form.style.display = "block";
+  greeting.style.display = 'none';
+})
+
+
+function init(){
+ askForName();
+};
+init();
